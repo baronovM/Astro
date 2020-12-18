d04a5338361567817061c123acfa03a928da4c58
baronov05@gmail.com <baronov05@gmail.com>
2020-12-18 18:53:17 +0400

    Пытаюсь создать класс NumColor с перегруженным умножением на число, но возникает непонятная ошибка

--- Astro/Header.hpp
+++ Astro/Header.hpp
@@ -1,13 +1,21 @@
 #include <SFML/Graphics.hpp>
 
-using namespace sf;
 using namespace std;
 
 #ifndef NumColor
 #define NumColor
 
-class NumColor() {
-
-}
+class NumColor {
+	sf::Uint8 r, g, b, a;
+public:
+	NumColor(sf::Uint8 r1, sf::Uint8 g1, sf::Uint8 b1, sf::Uint8 a1) {
+		r = r1;
+		g = g1;
+		b = b1;
+		a = a1;
+	}
+	NumColor operator*(const int& k) const {
+		return NumColor(r * k, g * k, b * k, a);
+};
 
 #endif
