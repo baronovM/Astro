436b3051bc5bb891193440a27ce744f2f9c3beb9
baronov05@gmail.com <baronov05@gmail.com>
2021-03-12 15:59:34 +0400

    Перенёс класс в main

--- Astro/reverse_distorce.cpp
+++ Astro/reverse_distorce.cpp
@@ -17,7 +17,38 @@ using namespace sf;
 
 #define guide "Используйте \"Reverse_Fisheye_Project.exe <k> [f] <image_path>\".\n"
 
+class NumColor : public Color {
+public:
+	NumColor();
 
+	NumColor(Color c);
+
+	NumColor(Uint8 r1, Uint8 g1, Uint8 b1, Uint8 a1);
+	NumColor operator*(const double& k) const;
+};
+
+
+NumColor::NumColor() {
+	r = 0; g = 0; b = 0; a = 0;
+}
+
+NumColor::NumColor(Color c) {
+	r = c.r;
+	g = c.g;
+	b = c.b;
+	a = c.a;
+}
+
+NumColor::NumColor(Uint8 r1, Uint8 g1, Uint8 b1, Uint8 a1) {
+	r = r1;
+	g = g1;
+	b = b1;
+	a = a1;
+}
+
+NumColor NumColor::operator*(const double& k) const {
+	return NumColor(r * k, g * k, b * k, a);
+}
 
 double sign(double x) {
 	return (x >= 0.0 ? 1.0 : -1.0);
