75b878bd3825cd562ee6de6344736ec3845e66eb
Андрей Дмитриевич Власов <vlasov.ad@gmail.com>
2021-03-12 17:30:11 +0400

    distorce.exe работает

--- Astro/distorce.cpp
+++ Astro/distorce.cpp
@@ -66,11 +66,28 @@ Color interpolation(double x, double y, Image image) {
 	return pixel;
 }
 
-int main() {
+int main(int argc, char** argstr) {
 	setlocale(LC_ALL, "RUS");
 	double k;	// Интенсивность коррекции.
 	double f;	// Фокусное расстояние, по умолчанию - "чтобы экватор сферы был вписан в наименьший размер изображения".
 
+/*
+    string imagePath, outImagePath;
+	bool silent = 0;
+	if (argc == 1) {
+		cout << "Введите коэффициент k, названия входного и выходного изображений - {k} {Название входного, без пробелов и с расширением} {Название выходного}:\n";
+		cin >> k >> imagePath >> outImagePath;
+	}
+	else {
+		k = atoi(argstr[1]) / 100.0;
+		imagePath = argstr[2];
+		outImagePath = argstr[3];
+		if (argc == 5)
+			silent = atoi(argstr[4]);
+		cout << argstr[1];
+		cout << k << endl;
+	}
+*/
 	string imagePath, outImagePath;
 	cout << "Введите коэффициент k, названия входного и выходного изображений - {k} {Название входного, без пробелов и с расширением} {Название выходного}:\n";
 	cin >> k >> imagePath >> outImagePath;
